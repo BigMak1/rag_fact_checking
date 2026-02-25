16a4cb203b42f8ab15cff781e66d40576f3c8a45
BigMak1 <mkonovalov9876@mail.ru>
2026-02-26 00:10:19 +0300

    Create 2 steps: DeReC and DRAGON

---
+++ DRAGON/rag_bench/constants.py
@@ -0,0 +1,7 @@
+LOG_FILE = "log.txt"
+
+PUBLIC_TEXTS_REPO_ID = "ai-forever/rag-bench-public-texts"
+PUBLIC_QUESTIONS_REPO_ID = "ai-forever/rag-bench-public-questions"
+HIST_TEXTS_REPO_ID = "ai-forever/hist-rag-bench-public-texts"
+HIST_QUESTIONS_REPO_ID = "ai-forever/hist-rag-bench-public-questions"
+THINK_END_TOKEN = "</think>"
